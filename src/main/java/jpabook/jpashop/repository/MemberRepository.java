715a69c02de9d625a4d17f09ecba635a8148e82e
LeeJaeHyeok97 <hazardous10@naver.com>
2024-04-14 00:05:35 +0900

    JPA활용1 완강

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PersistenceUnit;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor // final이 붙은 변수들에 필요한 생성자를 주입해준다.
+public class MemberRepository {
+
+    private final EntityManager em;
+
+//    @PersistenceUnit   em팩토리를 주입 받는 방법.
+//    private EntityManagerFactory entityManagerFactory;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        List<Member> result = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        return result;
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
